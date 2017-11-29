aac88efd08551e2ab06b45110842005e187c9f63
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2017-11-29 15:00:27 +0530

    Rename positive or negative.java to Prn.java

---
+++ Prn.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Positiveornegative
+{
+public static voiid main(String args[])
+{
+int a;
+System.out.println(" enter the number");
+Scanner obj=new Scanner(System.in);
+a=obj.nextInt();
+if(a>=0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("negative");
+}
+}
+}
