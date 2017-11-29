6beb82d647df307b66363a3b954bed2dd1acd3f1
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2017-11-29 14:15:36 +0530

    Rename Ero.java to Oddreven.java

---
+++ Oddreven.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Evenorodd
+{
+public static voiid main(String args[])
+{
+int a;
+System.out.println(" enter the number");
+Scanner obj=new Scanner(System.in);
+a=obj.nextInt();
+if(a%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
