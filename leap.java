5b4ab7fc5c54b4dfd88e9b9c203d36c13020f5b3
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2017-11-30 08:09:14 +0530

    Update and rename Oddreven.java to leap.java

--- leap.java
+++ leap.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Evenorodd
+class Leap
 {
 public static void main(String args[])
 {
@@ -7,13 +7,13 @@ int a;
 System.out.println(" enter the number");
 Scanner obj=new Scanner(System.in);
 a=obj.nextInt();
-if(a%2==0)
+if(a%4==0)
 {
-System.out.println("even");
+System.out.println("leap year");
 }
 else
 {
-System.out.println("odd");
+System.out.println("not leap year");
 }
 }
 }
