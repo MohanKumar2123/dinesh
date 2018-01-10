6d3303393403b86ede22745dcaf8081ebb49247a
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-01-10 15:42:05 +0530

    Update sumarray.java

--- sumarray.java
+++ sumarray.java
@@ -4,9 +4,10 @@ public class sumarray
 public staic void main(String args[])
 {
 Scanner in=new Scanner(System.in);
-  int array[]=new int[n];
+  
   int n=in.nextInt();
   int k=in.nextInt();
+  int array[]=new int[n];
   int sum=0;
   for(n=0;n<k;n++)
   {
