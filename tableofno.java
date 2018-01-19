ff03afa699f3c15b20e27bf41360be05885121b3
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-01-19 16:21:10 +0530

    Create tableofno.java

---
+++ tableofno.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class tableofno
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int no=in.nextInt();int mul;
+for(int i=1;i<=5;i++)
+{
+mul=i*no;
+System.out.println(+mul);
+}
+}
+}
