9c17d6f6b14363441647b373d08614aaa21a5bd4
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-01-20 10:50:26 +0530

    Rename armstrongno.java to armstrongnumber.java

---
+++ armstrongnumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class armstrong
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int no=in.nextInt();
+while(no>0)
+{
+i=no%10;
+sum=sum+i*i*i;
+no=no/10;
+}
+if(temp==sum)
+{
+
+}
+}
+}
