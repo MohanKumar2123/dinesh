db6a3151e46425f5f8f07b7f9a8bc104734b4feb
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-01-26 20:58:50 +0530

    Update beg32.java

--- beg32.java
+++ beg32.java
@@ -3,11 +3,12 @@ public class Numeric {
 
 public static void main(String[] args) {
 Scanner in=new Scanner(System.in);
-        int count=0;
-String s1;
-        s1=in.nextLine();
-       for(int i=1;i<=s1.length();i++){
-           if(((s1.charAt(i))==" ")&&((s1.charAt(i+1))!="")){
+        int count=1;
+String s;
+        s=in.nextLine();
+       for(int i=0;i<s.length()-1;i++){
+         	if ((s.charAt(i) == ' ') && (s.charAt(i + 1) != ' '))
+{
                count++;
            }
        }
