8d92c0e6111d4653f4937586f745edc4f19755e7
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-01-30 10:51:09 +0530

    Create beg41.java

---
+++ beg41.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class demo
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        String s=in.nextLine();
+        int n=in.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            System.out.println(s);
+        }
+    }
+}
