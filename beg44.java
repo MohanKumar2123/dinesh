dfc8953d53485b41d688e87dfcf86b2859a535fb
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-01-31 17:03:33 +0530

    Create beg44.java

---
+++ beg44.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ public class powertwo{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        if((a>=1)&&(a<=10))
+        System.out.println("Yes");
+        else
+        System.out.println("no");
+
+    }
+}
+ 
+
