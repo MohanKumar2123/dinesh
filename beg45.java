372d4fcebbfced4c07c015303b1b1a6f32d937a3
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-01-31 17:31:29 +0530

    Create beg45.java

---
+++ beg45.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class digit
+{
+public static void main(String[] args)
+{
+Scanner in=new Scanner(System.in);
+int no=in.nextInt();
+int digit=0;
+while(no!=0)
+{
+no/=10;
+++digit;
+}
+System.out.println("no of digits"+digit);
+}}
