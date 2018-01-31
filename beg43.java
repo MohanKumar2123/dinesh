4c7fd3bec36429a048d96a2e87e44c1b92438020
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-01-31 17:21:36 +0530

    Create beg43.java

---
+++ beg43.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+ public class powertwo{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        String s1=in.nextLine(),s2=in.nextLine();
+        System.out.println(s1+" "+s2);
+        }
+}
+ 
+
