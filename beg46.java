c2430d89b3aee72b8e7620e400b6e7dbf57a4164
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-01-31 15:44:47 +0530

    Create beg46.java

---
+++ beg46.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class next
+{
+public static void main(String args[]){
+    Scanner in=new Scanner(System.in);
+    int no= in.nextInt();
+    int b;
+    b=no+1;
+    System.out.println("next number is"+b);
+}}
