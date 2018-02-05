87b51ae34b73261800b67a6a9304cbd4f11127d7
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-05 16:53:59 +0530

    Create beg57.java

---
+++ beg57.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class sum
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int no1=in.nextInt(),no2=in.nextInt(),t;
+        t=no1;
+        no1=no2;
+        no2=t;
+        System.out.println(no1+" "+no2);
+    }}
