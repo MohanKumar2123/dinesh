dee78acbb1654cc14be25efc7805ac53cda19c57
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-06 20:57:52 +0530

    Update beg59.java

--- beg59.java
+++ beg59.java
@@ -2,16 +2,16 @@ import java.util.*;
 public class special
 { public static void main(String[] args)
     {
-        int large=0; 
+        int max=0; 
         int num;
         Scanner in=new Scanner(System.in);
         for(int i=0;i<10;i++)
         {
           num=in.nextInt();
-          if(num>large)
+          if(num>max)
           {
-           large=num;
+           max=num;
           }}
-          System.out.println("the largest is:"+large);
+          System.out.println("the largest  number is:"+max);
         }
 }
