7343c294fc0e3fe91be9eaa47d8323c35a35d10a
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-07 16:52:46 +0530

    Create beg70.java

---
+++ beg70.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class number
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+     int no=in.nextInt();
+        if(no%7==0)
+        System.out.println("yes");
+        else
+         System.out.println("no");
+    }
+}
