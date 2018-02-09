bb4add8e7cf63e6077871c48fa31592d7cddcf32
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-09 22:28:53 +0530

    Create beg81.java

---
+++ beg81.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+    Scanner in=new Scanner(System.in);
+    for(int i=1;i<=10;i++)
+    {
+    int no1=in.nextInt(),no2=in.nextInt();
+System.out.println(+(no1-no2));}
+    }
+}
