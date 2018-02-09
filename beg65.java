05ebd9eb9d367c40cb48d3dfdc1e9c53bc0b345a
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-09 15:40:50 +0530

    Create beg65.java

---
+++ beg65.java
@@ -0,0 +1,6 @@
+public class MyClass {
+    public static void main(String args[]) {
+    if(System.out.printf("hello world\n")==null){}
+    if(System.out.append("hello world").equals(null)){}
+    }
+}
