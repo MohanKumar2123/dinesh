760f865390dd51dbc752e03a5b2c0bcfd96c807b
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-09 21:27:55 +0530

    Create beg76.java

---
+++ beg76.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+    Scanner in=new Scanner(System.in);
+    int no=in.nextInt();boolean count=true;
+    for(int i=2;i<no/2;i++)
+    {
+        if(no%i==0)
+        {
+            count=false;
+            break;
+        }
+        
+    }
+    if(count==false)
+    {
+        System.out.println("yes");
+    }
+    else
+    {
+        System.out.println("no");
+    }
+    
+    }
+}
