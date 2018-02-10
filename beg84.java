16192ed4afabf3e3dbbc786d784e40b9ab17cb92
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-10 07:27:19 +0530

    Create beg84.java

---
+++ beg84.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Check{  
+public static void main(String[] args) { 
+    Scanner in=new Scanner(System.in);
+        char ch = in.next().charAt(0);
+        int asc= (int) ch; 
+        System.out.println(ch+" = "+ asc); 
+        }}
