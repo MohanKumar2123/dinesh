d0114655eb6a2859d627fbe4d55572be7d0f3d26
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-12 16:06:35 +0530

    Create pla5.java

---
+++ pla5.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+    Scanner in=new Scanner(System.in);
+    String s=in.nextLine();
+    char ch[]=s.toCharArray();
+    for(int i=0;i<s.length();i++)
+    {
+        if(ch[i]=='I')
+        {c1=1;}
+        else
+        {c1=0;}
+        if()
+        
+    }
+        System.out.println(s);
+    }
+}
