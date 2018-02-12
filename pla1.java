f70c67c9b17f4681aff9911648ff105707de5146
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-12 16:40:54 +0530

    Create pla1.java

---
+++ pla1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+    Scanner in=new Scanner(System.in);
+    String s=in.nextLine();
+    StringBuffer ss=new StringBuffer();
+    ss.append(s);
+    ss.reverse();
+        System.out.println(ss);
+        
+    }}
+
