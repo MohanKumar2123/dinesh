5964cc0cdf9db4c0eea355681176b7ec17f860db
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-12 15:49:02 +0530

    Create pla3.java

---
+++ pla3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+    Scanner in=new Scanner(System.in);
+    int no=in.nextInt(),revno=0;
+    while(no>0)
+    {
+    revno=revno*10+no%10;
+    no/=10;
+    }
+        System.out.println(+revno);
+    }
+}
