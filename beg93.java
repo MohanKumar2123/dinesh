10f7a78c3c42c64c09b5d1a882163f3828b082fb
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-14 18:47:01 +0530

    Create beg93.java

---
+++ beg93.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[]) 
+    {
+        Scanner in=new Scanner(System.in);
+        int cel=in.nextInt(),kel=cel+273;
+        System.out.println(+kel);
+    }
+}
