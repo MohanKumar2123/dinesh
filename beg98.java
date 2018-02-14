2bbff0d328b8e445794e1727aefd9c196798875f
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-14 19:09:49 +0530

    Create beg98.java

---
+++ beg98.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[]) 
+    {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt(),b=in.nextInt(),c=in.nextInt();
+      System.out.println(a*b%c);
+    }
+}
