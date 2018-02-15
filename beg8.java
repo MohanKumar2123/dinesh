c74d64b230327af9d990cc1c6994b99526bc36f4
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-15 13:32:46 +0530

    Create beg8.java

---
+++ beg8.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sum{
+public static void main(String args[])
+{
+int i=1,n,total=0;
+Scanner in=new Scanner(System.in);
+n=in.nextInt();
+for(i=1;i<=n;i++){
+total=total+i;}
+System.out.println(total);}}
