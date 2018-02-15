5d9a958f4249d377ea910ce85c5ed4f0b1e5ce52
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-15 12:30:20 +0530

    Create beg116.java

---
+++ beg116.java
@@ -0,0 +1,8 @@
+
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+    Scanner in=new Scanner(System.in);
+   int N=in.nextInt(),K=in.nextInt();
+   System.out.println(N+""+K);
+}}
