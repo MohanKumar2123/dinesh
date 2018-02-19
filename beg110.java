92cd28accbdd3da5715c8f957d30ecb7003d3260
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-19 22:53:15 +0530

    Create beg110.java

---
+++ beg110.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner in=new Scanner(System.in);
+        int  n=in.nextInt(),sum=0;
+        for(int i=1;i<=n;i++)
+        {
+        sum=sum+i;}
+            System.out.println(sum);
+}}
