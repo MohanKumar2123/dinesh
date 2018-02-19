64f5d51842aea4616118426c8abb0d892f268e15
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-19 23:04:31 +0530

    Create beg105.java

---
+++ beg105.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();String s="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char ch[]=s.toCharArray();
+             System.out.println(ch[a-1]);
+            }}
