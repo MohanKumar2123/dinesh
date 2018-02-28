627b3573a33ad5b38a58ef46988f7fc08fe5ef2d
tmdineshcity <31897346+tmdineshcity@users.noreply.github.com>
2018-02-28 15:46:29 +0530

    Create beg104.java

---
+++ beg104.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.ArrayList;
+public class MyClass {
+    public static void main(String args[]) {
+    Scanner in=new Scanner(System.in);
+    double N=in.nextDouble(),K=in.nextDouble();
+ System.out.println(+Math.pow(N,K));
+    }
+}
